af62c793654b1176aca2e096a3c1c3e176b22c48
DESKTOP-CTHDH3M\Steve <raistlin2015@gmail.com>
2018-11-10 14:05:41 -0700

    fix overflow hidden

--- angular/src/app/components/navbar/navbar.component.ts
+++ angular/src/app/components/navbar/navbar.component.ts
@@ -44,6 +44,7 @@ export class NavbarComponent implements OnInit, AfterViewInit {
     this.sideMenu.nativeElement.classList.remove("side-menu-fade-in");
     this.sideMenu.nativeElement.classList.add("side-menu-fade-out");
     this.sideMenuCloseButton.nativeElement.style.display = "none";
+    this.menuButton.nativeElement.style.display = "block";
     setTimeout(() => {
       this.sideMenu.nativeElement.classList.remove("side-menu-fade-out");
       this.sideMenu.nativeElement.classList.remove("side-menu-fade-in");
